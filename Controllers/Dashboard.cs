531392ef35d27d86e44315a19cb45f297ec9e6e8
Mussab12 <shahidmussab7@gmail.com>
2023-03-19 16:59:11 +0500

    Dashboard_Starter_Template

---
+++ Controllers/Dashboard.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dashboard_Starter_Template.Controllers
+{
+    public class Dashboard : Controller
+    {
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dashboard_Starter_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
